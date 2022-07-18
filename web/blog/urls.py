780e6d36512cf6bca337adec549884e21b6472a9
data-datum <noelia0618@gmail.com>
2022-07-18 14:23:20 -0300

    bug en urls

--- web/blog/urls.py
+++ web/blog/urls.py
@@ -10,5 +10,5 @@ urlpatterns = [
     path('cursos/', cursos, name="cursos"),
     path('cv/', cv, name="cursos"),
     path('acerca/', acerca, name="acerca"),
-    path('post/', post, name="post.html")
+    path('post/', post, name="post")
 ]
